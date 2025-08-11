f81421853c76c9d5e6f3967b3c0294f1a8bdf336
n1x <pnipon74@gmail.com>
2025-08-11 11:42:57 +0300

    Expedition Antarctica.lua

--- Expedition Antarctica.lua
+++ Expedition Antarctica.lua
@@ -1,7 +1,7 @@
 local RS = game:GetService('ReplicatedStorage')
 local JobId = game.JobId
 local PlaceId = game.PlaceId
-local Remote = game:GetService("ReplicatedStorage"):WaitForChild("ClaimReward")
+local Remote = RS:WaitForChild("ClaimReward")
 local TeleportService = game:GetService("TeleportService")
 local function Fire(obj)
     obj:FireServer()
