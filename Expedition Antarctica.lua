a7922b5109fa18bc5c8b48f1ecd9698ab2d85613
n1x <pnipon74@gmail.com>
2025-08-14 20:26:51 +0300

    Expedition Antarctica.lua

--- Expedition Antarctica.lua
+++ Expedition Antarctica.lua
@@ -3,6 +3,7 @@ local JobId = game.JobId
 local PlaceId = game.PlaceId
 local Remote = RS:WaitForChild("ClaimReward")
 local TeleportService = game:GetService("TeleportService")
+local Stat = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("Altimeter"):WaitForChild("ImageLabel"):WaitForChild("Coin_Image"):WaitForChild("Coin_Amount")
 
 local function Fire(obj)
     obj:FireServer()
@@ -12,13 +13,6 @@ Fire(Remote)
 
 queue_on_teleport([[loadstring(game:HttpGet("https://raw.githubusercontent.com/scytheXlol/Scripts/refs/heads/main/Expedition%20Antarctica.lua"))()]])
 
-local player = game:GetService("Players").LocalPlayer
-local playerGui = player:WaitForChild("PlayerGui")
-local altimeter = playerGui:WaitForChild("Altimeter")
-local imageLabel = altimeter:WaitForChild("ImageLabel")
-local coinImage = imageLabel:WaitForChild("Coin_Image")
-local coinAmount = coinImage:WaitForChild("Coin_Amount")
-
-coinAmount.Changed:Connect(function(Value)
+Stat.Changed:Connect(function(Value)
     TeleportService:TeleportToPlaceInstance(PlaceId, JobId)
 end)
